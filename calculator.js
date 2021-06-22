2ac9fcd770c02d421e470f0329ce24464233674a
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-06-22 11:43:55 -0500

    finalizado

--- calculator.js
+++ calculator.js
@@ -1,18 +1,32 @@
+console.log("Hola")
+
 function sum(a, b) {
-  return;
+  if (a === undefined) {
+    return 0
+  }  
+  return a + b;
 }
 
 function subtract(a, b) {
-  return;
+
+  if (a === undefined) {
+    return 0
+  }
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+  if (b === 0){
+    throw console.error("Error");
+  }
+  return a / b;
 }
 
 function multiply(a, b) {
   return a * b;
 }
+
+console.log(sum(13,15))
 
 // The following is required to make unit tests work. Please ignore it.
 module.exports = { sum, subtract, divide, multiply };
